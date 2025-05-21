59557ee6fccfec7408daed76ce1528aa4bc035c5
elphie-pro <onazjr2006@gmail.com>
2025-05-21 21:17:44 +0100

    buttons

--- components/Home/Heading.vue
+++ components/Home/Heading.vue
@@ -9,12 +9,16 @@
         style="font-family: 'Cakecafe';"
       />
     </h1>
-     <div class=" flex gap-3  pl-[9rem] text-[1.64rem]"> 
+     <div class=" flex gap-3 pt-[2rem]  pl-[9rem] text-[1.64rem]"> 
             <Motion is="p" preset="slideVisibleBottom" :duration="800" class="">Software Engineer ||</Motion> 
             <Motion is="p" preset="slideVisibleBottom" :duration="1000" >Frontend Developer ||</Motion> 
             <Motion is="p" preset="slideVisibleBottom" :duration="1200" >UI/UX Designer ||</Motion> 
             <Motion is="p" preset="slideVisibleBottom" :duration="1400" >Photographer </Motion> 
         </div>
+      <div class="flex flex-col gap-7 md:flex-row pt-[2.5rem] lg:pl-[28rem]">
+        <button class="w-[10rem] h-[2.5rem] bg-[#f4f4f4] text-[#263238] text-[1.2rem] rounded-2xl">Contact Me</button>
+        <button class="w-[10rem] h-[2.5rem] border-[#f4f4f4] text-[#f4f4f4] border text-[1.2rem] rounded-2xl">My Resume</button>
+      </div>
     </div>
 </template>
 
