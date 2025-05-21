1fe339c125dbb58f77018304ed6f320a52c6b107
elphie-pro <onazjr2006@gmail.com>
2025-05-21 20:52:51 +0100

    profile incomplete

--- components/Home/Text.vue
+++ components/Home/Text.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full block max-w-[80rem] mx-auto px-4 py-6 text-center overflow-hidden text-[#f4f4f4]">
-         <div class="flex gap-3 pl-[9rem] text-[2rem]">
+         <div class="w-full flex gap-3 pl-[9.5rem] text-[1.5rem]">
             <Motion is="h2" preset="slideVisibleBottom" :duration="800" >Software Engineer ||</Motion> 
             <Motion is="h2" preset="slideVisibleBottom" :duration="1000" >Frontend Developer ||</Motion> 
             <Motion is="h2" preset="slideVisibleBottom" :duration="1200" >UI/UX Designer ||</Motion> 
