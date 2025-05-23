a0a257c15c0959b65803d0c7c270d7ddca691843
elphie-pro <onazjr2006@gmail.com>
2025-05-23 23:18:52 +0100

    project incomplete

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,6 +1,6 @@
 <template>
-    <div w-full block max-w-[80rem] mx-auto px-4 py-6 text-center overflow-hidden text-[#f4f4f4] mt-[-2rem]>
-
+    <div class="w-full block max-w-[80rem] mx-auto px-4 py-6  overflow-hidden text-[#f4f4f4] text-[2.5rem] text-center">
+        <h2 style="font-family: 'Cakecafe';">My Recent Projects</h2>
     </div>
 </template>
 
