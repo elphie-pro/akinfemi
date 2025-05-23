c0ad532b78f9bc1fc4c21fb1538fc6bcc1517b3d
elphie-pro <onazjr2006@gmail.com>
2025-05-23 23:16:30 +0100

    project incomplete

---
+++ components/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+    <div w-full block max-w-[80rem] mx-auto px-4 py-6 text-center overflow-hidden text-[#f4f4f4] mt-[-2rem]>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,6 +2,7 @@
     <div class="bg-[#263238] h-screen w-screen overflow-hidden">
         <Navbar />
         <Background />
+        <Projects />
     </div>
 </template>
 
