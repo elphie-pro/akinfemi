3a9c30de7565258344c79ea5597bac937bab9e50
elphie-pro <onazjr2006@gmail.com>
2025-05-26 19:19:37 +0100

    project incomplete

--- components/Project.vue
+++ components/Project.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="w-full block max-w-[90rem] mx-auto px-4 py-6  overflow-x-hidden text-[#f4f4f4]  ">
-        <h2 class="text-center text-[2.5rem]" style="font-family: 'Cakecafe';">My Recent pjs</h2>
+        <h2 class="text-center text-[2.5rem]" style="font-family: 'Cakecafe';">My Recent Projects</h2>
         <div class="grid grid-cols-3">
              <div v-for="pj in projects" :key="pj.name">
                 <SpotlightCard  class="my-4 w-[28rem] h-[30rem] text-white border-white border-8 " style="background: conic-gradient(from 210deg, #367898 0deg, #3B8DB7 160deg, #2e4f5f 30deg, #29353B 360deg);" spotlightColor="rgba(200, 200, 255, .8)" @click = "show = true">
-            <div class="flex flex-col gap-5">
+            <div class="flex flex-col gap-4">
                 <img :src="pj.img" class="w-full"/>
-                <h1 class="font-custom text-[2rem] text-[#f4f4f4]">{{ pj.title }}</h1>
+                <h1 class="font-custom text-[2rem] text-[#f4f4f4] font-black">{{ pj.title }}</h1>
                 <div class="flex justify-between items-center">
                 <h2>Tech Stack:</h2>
                 <p class="text-[.8rem] w-[16rem]">{{ pj.stack }}</p>
@@ -39,27 +39,27 @@ const images = import.meta.glob('../assets/images/*', { eager: true, query: '?ur
 
 const projects = [
     {
-        title: 'ElphBlog',
-        img: images['../assets/images/Screenshot 2025-05-15 102407.png'],
-        stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
+        title: 'Akinfemi V1',
+        img: images['../assets/images/Screenshot 2025-05-26 190840.png'],
+        stack: 'Nuxt.JS, JavaScript, Tailwind CSS, Vueuse Motion',
         descript: 'ElphBank is a makeshift banking site... Read more',
         site:'https://elph-blog.vercel.app/',
-        code: 'https://github.com/elphie-pro/ElphBlog'
+        code: 'https://github.com/elphie-pro/akinfemi'
     },
     {
-        title: 'ElphBlog',
-        img: images['../assets/images/Screenshot 2025-05-15 102407.png'],
+        title: 'ElphBank',
+        img: images['../assets/images/Screenshot 2025-05-15 195231.png'],
         stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
         descript: 'ElphBank is a makeshift banking site... Read more',
-        site:'https://elph-blog.vercel.app/',
-        code: 'https://github.com/elphie-pro/ElphBlog'
+        site:'',
+        code: 'https://github.com/elphie-pro/ElphBank'
     },
     {
-        title: 'ElphBlog',
+        title: 'ElphBlog V1',
         img: images['../assets/images/Screenshot 2025-05-15 102407.png'],
-        stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
-        descript: 'ElphBank is a makeshift banking site... Read more',
-        site:'https://elph-blog.vercel.app/',
+        stack: 'React.JS, JavaScript, CSS, Firebase Auth, Firestore',
+        descript: 'ElphBlog is a blogging website that allows... Read more',
+        site:'',
         code: 'https://github.com/elphie-pro/ElphBlog'
     },
     {
