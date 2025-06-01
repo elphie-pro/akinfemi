8e3557f810dd315f79ed83616d1720be38746c12
elphie-pro <onazjr2006@gmail.com>
2025-06-01 21:52:49 +0100

    about incomplete

--- components/Abou.vue
+++ components/Abou.vue
@@ -2,13 +2,15 @@
     <div class="w-full block max-w-[80rem] mx-auto px-4 py-6  overflow-x-hidden text-[#f4f4f4]  ">
         <h2 class="text-center text-[3.5rem]" style="font-family: 'Cakecafe';">Who is Akinfemi?</h2>
 
-        <div class="flex justify-between pt-[7rem]">
-            <div class="justify-center items-center w-[35rem]">
+        <div class="flex justify-between pt-[2rem]">
+            <div class="justify-center items-center w-[35rem] pt-[9rem]">
                 <h1 class="text-center text-[3.5rem]" style="font-family: 'Cakecafe';">“The only way to do great work to to love what you do”</h1>
             </div>
             <div class="text-[1.2rem] flex flex-col gap-5 text-center">
                 <h2> Onadeko Akinfemi is a self-taught and highly motivated <br> software engineer with solid experience in frontend <br> development of systems that scale to a lot of users.</h2>
                 <h2>My coding journey began in the 8th grade in high school <br> when many of my schoolmates were making websites for <br> fun, which caught my attention. Unlike others, I didn't <br> eventually drop web development; instead, it continuously <br> piqued my interest and made me want to dive deeper into <br> the world of tech. My career as a developer started <br> officially in the 9th grade when I could confidently build <br> different websites by myself and i eventually started <br> working with JavaScript.</h2>
+                <h2>Today, with expertise in Frontend development and UI/ <br>UX design, combined with resilience and determination, <br> along with skills in various technologies like React.js, <br> Vue.js, Figma, and more, I've had the opportunity to <br> collaborate with great minds to engineer solutions to <br> problems.</h2>
+                <h2>I'm always excited to take on challenging problems head- <br> on, using the tools in my arsenal to positively impact  the <br> world with ones and zeros.</h2>
             </div>
         </div>
     </div>
