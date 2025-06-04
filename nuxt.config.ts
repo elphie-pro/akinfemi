a4bead4b77dbfd2a15fcf119cd43c310bb2e92dc
elphie-pro <onazjr2006@gmail.com>
2025-06-04 14:18:58 +0100

    about incomplete

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+   app: {
+    head: {
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+    }
+  },
   ssr: true, // Make sure SSR is enabled
   router: {
     options: {
