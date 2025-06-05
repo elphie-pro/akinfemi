ac833936825732df9722a02bb9363dca2885931c
ThisDevIsTired <michaelamao442@gmail.com>
2025-06-06 00:35:55 +0100

    Size fix

--- components/Home/Heading.vue
+++ components/Home/Heading.vue
@@ -1,28 +1,56 @@
 <template>
-    <div class="w-full block lg:max-w-[80rem] md:max-w-[45rem] mx-auto px-4 py-6 text-center overflow-x-hidden text-[#f4f4f4] md:mt-[3rem] lg:mt-auto">
-         <h1>
+  <div
+    class="w-full block lg:max-w-[80rem] md:max-w-[45rem] mx-auto px-4 py-6 text-center overflow-x-hidden text-[#f4f4f4] md:mt-[3rem] lg:mt-auto"
+  >
+    <h1>
       <DecryptedText
         text="Hi, I'm Onadeko Akinfemi Raphael"
         animate-on="view"
-        class-name="text-[#f4f4f4] md:text-[3.5rem] lg:text-[5rem] sm:text-[1rem] text-[1.8rem] font-bold text-center"
+        class-name="text-[#f4f4f4] md:text-[3rem] lg:text-[3.7rem] min-[420px]:text-[1.8rem] text-[1.5rem] font-bold text-center"
         encrypted-class-name="text-[#f4f4f4] md:text-[3.5rem] lg:text-[5rem] sm:text-[1rem]"
-        style="font-family: 'Cakecafe';"
+        style="font-family: 'Cakecafe'"
       />
     </h1>
-     <div class=" flex gap-3 lg:pt-[-2rem] md:pt-[2rem] text-center justify-center lg:text-[1.64rem] md:text-[1.15rem] text-[.6rem]"> 
-            <Motion is="p" preset="slideVisibleBottom" :duration="800" class="">Software Engineer ||</Motion> 
-            <Motion is="p" preset="slideVisibleBottom" :duration="1000" >Frontend Developer ||</Motion> 
-            <Motion is="p" preset="slideVisibleBottom" :duration="1200" >UI/UX Designer ||</Motion> 
-            <Motion is="p" preset="slideVisibleBottom" :duration="1400" >Photographer </Motion> 
-        </div>
-      <motion  is="div" preset="slideVisibleBottom" :duration="1800" class="flex gap-7 md:flex-row pt-[2.5rem] justify-center">
-        <motion  is="button" preset="slideVisibleLeft" :duration="2000" class="md:w-[10rem] md:h-[2.5rem] w-[7rem] h-[2.5rem] bg-[#f4f4f4] text-[#263238] text-[1rem] md:text-[1.2rem] rounded-xl md:rounded-2xl cursor-pointer">Contact Me</motion>
-        <motion  is="button" preset="slideVisibleRight" :duration="2000" class="md:w-[10rem] md:h-[2.5rem] w-[7rem] h-[2.5rem] border-[#f4f4f4] text-[#f4f4f4] border text-[1rem] md:text-[1.2rem] rounded-xl md:rounded-2xl cursor-pointer">My Resume</motion>
-      </motion>
+    <div
+      class="flex gap-3 lg:pt-[-2rem] md:pt-[2rem] text-center justify-center lg:text-[1.64rem] md:text-[1.15rem] text-[.6rem]"
+    >
+      <Motion is="p" preset="slideVisibleBottom" :duration="800" class=""
+        >Software Engineer ||</Motion
+      >
+      <Motion is="p" preset="slideVisibleBottom" :duration="1000"
+        >Frontend Developer ||</Motion
+      >
+      <Motion is="p" preset="slideVisibleBottom" :duration="1200"
+        >UI/UX Designer ||</Motion
+      >
+      <Motion is="p" preset="slideVisibleBottom" :duration="1400"
+        >Photographer
+      </Motion>
     </div>
+    <motion
+      is="div"
+      preset="slideVisibleBottom"
+      :duration="1800"
+      class="flex gap-7 md:flex-row pt-[2.5rem] justify-center"
+    >
+      <motion
+        is="button"
+        preset="slideVisibleLeft"
+        :duration="2000"
+        class="md:w-[10rem] md:h-[2.5rem] w-[7rem] h-[2.5rem] bg-[#f4f4f4] text-[#263238] text-[1rem] md:text-[1.2rem] rounded-xl md:rounded-2xl cursor-pointer"
+        >Contact Me</motion
+      >
+      <motion
+        is="button"
+        preset="slideVisibleRight"
+        :duration="2000"
+        class="md:w-[10rem] md:h-[2.5rem] w-[7rem] h-[2.5rem] border-[#f4f4f4] text-[#f4f4f4] border text-[1rem] md:text-[1.2rem] rounded-xl md:rounded-2xl cursor-pointer"
+        >My Resume</motion
+      >
+    </motion>
+  </div>
 </template>
 
 <script setup>
-import DecryptedText from '../components/DecryptedText.vue';
+import DecryptedText from "../components/DecryptedText.vue";
 </script>
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,26 @@
 <template>
   <Head>
-      <Title>Akinfemi</Title>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <Title>Akinfemi</Title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
   </Head>
 
-  <div class="bg-[#263238] lg:h-screen h-full md:h-screen w-screen overflow-x-hidden pb-0">
+  <div
+    class="bg-[#263238] lg:min-h-screen min-h-screen md:min-h-screen overflow-x-hidden"
+  >
     <Navbar />
     <Background />
     <Heading />
     <Skills />
-</div>
+  </div>
 </template>
 
 <script setup>
-import Background from '../components/Background.vue';
-import Heading from '../components/Home/Heading.vue'
-import Skills from '../components/Home/Skills.vue'
+import Background from "../components/Background.vue";
+import Heading from "../components/Home/Heading.vue";
+import Skills from "../components/Home/Skills.vue";
 
-const router = useRouter()
-
+const router = useRouter();
 </script>
