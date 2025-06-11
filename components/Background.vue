04a753b4b270302d7927bad8a3ba7f318a476f32
elphie-pro <onazjr2006@gmail.com>
2025-06-11 13:16:57 +0100

    about incomplete

--- components/Background.vue
+++ components/Background.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="overflow-y-hidden md:overflow-hidden">
-        <img src="../assets/images/Group 1.svg" alt="" class="absolute w-1/2 xl:w-auto bottom-0 right-0 z-0">
-        <img src="../assets/images/Man developer writing programming code.svg" alt="" class="absolute w-1/2 xl:max-w-[15rem]  mt-auto md:-bottom-[3rem] bottom-[-2.5rem] xl:bottom-0 -left-2">
-        <img src="../assets/images/Programming using version control in cloud.svg" alt="" class="absolute w-1/2 xl:max-w-[15rem] xl:min-w-[20rem] mt-[10rem] xl:mt-auto top-0 right-0">
+        <img src="../assets/images/Group 1.svg" alt="" class="fixed w-1/2 xl:max-w-[25rem] bottom-0 right-0 z-0">
+        <img src="../assets/images/Man developer writing programming code.svg" alt="" class="fixed w-1/2 xl:max-w-[15rem]  mt-auto md:-bottom-[3rem] bottom-[-2.5rem] xl:bottom-0 -left-2">
+        <img src="../assets/images/Programming using version control in cloud.svg" alt="" class="fixed w-1/2 xl:max-w-[17rem] mt-[11rem] xl:mt-auto top-0 right-0">
         <div class="md:w-[6rem] w-[4rem]  absolute">
             <div class="flex flex-col gap-4 items-center">
                 <div class="w-[.1rem] md:h-[8rem] h-[4rem] bg-[#000] xl:mt-[5rem] md:mt-[12rem] mt-[9rem]"></div>
