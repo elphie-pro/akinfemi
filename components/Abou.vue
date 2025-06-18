ac8aa5cd97d74b21698d7f27402ca531d05029e3
elphie-pro <onazjr2006@gmail.com>
2025-06-18 20:46:14 +0100

    darkmode  incomplete

--- components/Abou.vue
+++ components/Abou.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="w-full block max-w-[80rem] mx-auto px-4 py-6  overflow-x-hidden dark:text-[#f4f4f4] text-[#263238] ">
+    <div class="w-full block max-w-[65rem] mx-auto px-4 py-6  overflow-x-hidden dark:text-[#f4f4f4] text-[#263238] ">
         <h2 class="text-center text-[3.5rem] font-devonshire">Who is Akinfemi?</h2>
 
-        <div class="flex justify-between pt-[1rem]">
+        <div class="flex justify-between pt-[1rem] pb-[3rem]">
             <div class="justify-center items-center w-[35rem] pt-[9rem]">
                 <h1 class="text-center text-[4.5rem] font-devonshire">“The only way to do great work to to love what you do”</h1>
             </div>
-            <div class="text-[1.2rem] flex flex-col gap-5 text-center">
-                <h2> Onadeko Akinfemi is a self-taught and highly motivated <br> software engineer with solid experience in frontend <br> development of systems that scale to a lot of users.</h2>
-                <h2>My coding journey began in the 8th grade in high school <br> when many of my schoolmates were making websites for <br> fun, which caught my attention. Unlike others, I didn't <br> eventually drop web development; instead, it continuously <br> piqued my interest and made me want to dive deeper into <br> the world of tech. My career as a developer started <br> officially in the 9th grade when I could confidently build <br> different websites by myself and i eventually started <br> working with JavaScript.</h2>
-                <h2>Today, with expertise in Frontend development and UI/ <br>UX design, combined with resilience and determination, <br> along with skills in various technologies like React.js, <br> Vue.js, Figma, and more, I've had the opportunity to <br> collaborate with great minds to engineer solutions to <br> problems.</h2>
-                <h2>I'm always excited to take on challenging problems head- <br> on, using the tools in my arsenal to positively impact  the <br> world with ones and zeros.</h2>
+            <div class="text-[1.2rem] flex flex-col gap-3 text-center w-[30rem]">
+                <h2> Onadeko Akinfemi is a self-taught and highly motivated br> software engineer with solid experience in frontend br> development of systems that scale to a lot of users.</h2>
+                <h2>My coding journey began in the 8th grade in high school br> when many of my schoolmates were making websites for br> fun, which caught my attention. Unlike others, I didn't br> eventually drop web development; instead, it continuously br> piqued my interest and made me want to dive deeper into br> the world of tech. My career as a developer started br> officially in the 9th grade when I could confidently build br> different websites by myself and i eventually started br> working with JavaScript.</h2>
+                <h2>Today, with expertise in Frontend development and UI/ br>UX design, combined with resilience and determination, br> along with skills in various technologies like React.js, br> Vue.js, Figma, and more, I've had the opportunity to br> collaborate with great minds to engineer solutions to br> problems.</h2>
+                <h2>I'm always excited to take on challenging problems head- br> on, using the tools in my arsenal to positively impact  the br> world with ones and zeros.</h2>
             </div>
         </div>
         <div class="w-full block lg:max-w-[80rem] md:max-w-[45rem] mx-auto px-4 py-6 text-center overflow-hidden dark:text-[#f4f4f4] text-[#263238] lg:mt-[-2rem] mt-[1rem]">
@@ -23,7 +23,7 @@
             <Motion is="div" preset="slideVisibleBottom" :duration="1000" class="grid grid-cols-3 md:grid-cols-5 gap-4 p-4 "> 
             <div v-for="sk in Skills" :key="sk.name" class="flex flex-col gap-3 text-center">
                 <div class="flex flex-col gap-3 justify-center items-center">
-                    <div class="md:w-[7rem] md:h-[7rem] w-[5rem] h-[5rem] border border-[#f4f4f4] rounded-xl flex items-center justify-center">
+                    <div class="md:w-[7rem] md:h-[7rem] w-[5rem] h-[5rem] border dark:border-[#f4f4f4] border-[#263238] rounded-xl flex items-center justify-center">
                     <img :src="sk.img" alt="" class="max-w-[50%] max-h-[50%] md:max-w-[70%] md:max-h-[70%] object-contain" /> 
                     </div>
                     <h2 class="">{{ sk.name }}</h2>   
--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,9 @@
     <div class="dark:bg-[#263238] bg-[#f4f4f4] h-screen w-screen overflow-x-hidden">
         <Navbar />
         <Background />
-        <Abou />
+        <div class="w-full m-auto max-w-6xl">
+          <Abou />
+        </div>
     </div>
 </template>
 
