f0f891268840247c04baa0440724dcac2bd858f8
elphie-pro <onazjr2006@gmail.com>
2025-07-09 23:01:05 +0100

    darkmode  incomplete

--- components/Project.vue
+++ components/Project.vue
@@ -21,10 +21,10 @@
                 </div> 
             </div>
             </SpotlightCard>
-                <div v-if="show" class="fixed bg-black/10 min-h-screen z-10 w-screen flex justify-center items-center top-0 left-0 text-white overflow-scroll" @click="show = false">
-                    <div class="bg-[#263238]/40 h-auto ml-[-1rem] md:w-[65rem] w-auto p-8 px-18 flex flex-col  justify-between" @click.stop>
+                <div v-if="show" class="absolute bg-black/10 min-h-screen z-10 w-screen flex justify-center items-center top-0 left-0 text-white " @click="show = false">
+                    <div class="bg-[#263238]/40 md:h-auto ml-[-1rem] h-[35rem] w-[20rem] md:w-[65rem] p-8 px-18 flex flex-col  justify-between" @click.stop>
                             <img :src="selectedProject.img" class="w-full"/>
-                            <div class="flex gap-[9rem] pt-3">
+                            <div class="flex flex-col md:flex-row md:gap-[9rem] pt-3">
                                             <div class="flex flex-col gap-5 ">
                                         <div class="flex gap-4 items-center">
                                             <h2 class="text-[.9rem]">Project Title:</h2>
