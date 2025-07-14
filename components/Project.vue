f3d394db53765e20b3e2dcbbfae930c8a44c16ee
elphie-pro <onazjr2006@gmail.com>
2025-07-14 20:38:35 +0100

    darkmode  incomplete

--- components/Project.vue
+++ components/Project.vue
@@ -3,7 +3,7 @@
         <h2 class="text-center text-[3rem] font-devonshire dark:text-[#f4f4f4] text-[#263238]">My Recent Projects</h2>
         <div class="grid-cols-2 flex flex-wrap items-center justify-center xl:grid mx-9 gap-x-[5rem] lg:gap-x-[9rem] md:gap-x-[5rem] md:ml-[-.05rem] ml-[3rem] ">
              <div v-for="pj in projects" :key="pj.name" class="lg:ml-[3.5rem]">
-                <SpotlightCard  class="my-4 p-[.5rem] md:p-0 md:px-[2rem] md:py-[1rem] sm:w-96 w-[80vw] md:h-[31rem] text-white dark:border-white border-[#263238] dark:border-8  dark:conic-dark conic-light" spotlightColor="rgba(200, 200, 255, .8)" @click = "openModal(pj)">
+                <SpotlightCard  class="my-4 p-[.5rem] md:p-0 md:px-[2rem] md:py-[1rem] sm:w-96 w-[80vw] md:h-[30rem] text-white dark:border-white border-[#263238] dark:border-8  dark:conic-dark conic-light" spotlightColor="rgba(200, 200, 255, .8)" @click = "openModal(pj)">
             <div class="flex flex-col gap-2 py-2 md:py-0">
                 <img :src="pj.img" class="w-full"/>
                 <h1 class="font-custom md:text-[2rem] text-[1.5rem] dark:text-[#f4f4f4] text-[#263238] font-black">{{ pj.title }}</h1>
@@ -11,18 +11,18 @@
                 <h2 class="dark:text-[#f4f4f4] font-semibold text-[1rem] text-[#263238]">Stack:</h2>
                         <p class="text-[.8rem] font-semibold w-[16rem] dark:text-[#f4f4f4] text-[#263238]">{{ pj.stack }}</p>
                 </div>
-                <div class="flex gap-3">
+                <div class="flex gap-2 ml-[4rem]">
                             <div v-for="sk in pj.stackImages" :key="sk.name" class="flex  text-center">
                             <div class="flex  justify-center items-center">
-                                <div class="w-[2.5rem] h-[2.5rem] border dark:border-[#f4f4f4] border-[#263238] rounded-4xl p-1 flex items-center justify-center">
+                                <div class="w-[2.5rem] h-[2.5rem] border dark:border-[#f4f4f4] border-[#263238] rounded-4xl p-[rem] flex items-center justify-center">
                                 <img :src="sk.source" alt="" class="max-w-[60%] max-h-[60%] md:max-w-[60%] md:max-h-[60%] object-contain" /> 
                                 </div>
                             </div> 
                         </div>
                         </div>
-                <div class="flex md:gap-5 gap-2 items-center">
+                <div class="flex md:gap-3 gap-2 items-center">
                 <h2 class="dark:text-[#f4f4f4] font-semibold text-[#263238]">Description:</h2>
-                <p class="text-[.6rem] w-[16rem] font-semibold dark:text-[#f4f4f4] text-[#263238]">{{ pj.descript }}</p>
+                <p class="text-[.7rem] w-[16rem] font-semibold dark:text-[#f4f4f4] text-[#263238]">{{ pj.descript }}</p>
                 </div>
                 <div class="flex justify-between">
                     <NuxtLink :to="pj.site" @click.stop :class="pj.site === '' ? 'flex items-center gap-2 text-[#b1afaf] line-through' : 'flex items-center gap-2 text-[#b1afaf]'">View Live Site <img src="../assets/images/send-sqaure-2.svg" alt="" class="w-[1.5rem]"></NuxtLink>
@@ -86,25 +86,20 @@ const projects = [
         title: 'Akinfemi V1',
         img: images['../assets/images/Screenshot 2025-05-26 190840.png'],
         stack: 'Nuxt.JS, JavaScript, Tailwind CSS, Vueuse Motion',
-        descript: 'Akinfemi V1 is a portfolio website... Read more',
-        description:'The Akinfemi project is a modern portfolio website built with Nuxt.js 3 that showcases personal projects and technical skills through an interactive, animated interface featuring a project gallery with Project.vue:6 SpotlightCard components that display seven different applications including "Akinfemi V1", "ElphBank", "ElphBlog V1", and others Project.vue:61-133 , sophisticated text animations through the DecryptedText.vue:77-81 DecryptedText component with character-by-character decryption effects, a responsive navigation system with Navbar.vue:8-16 route-aware styling, and a component-driven architecture that follows Nuxt.js file-based routing conventions with modular components for spotlight cards, animated text, and project displays, all built using Vue.js 3, Tailwind CSS, and @vueuse/motion for animations with server-side rendering capabilities.',
+        descript: 'Built the portfolio website youre looking at with Nuxt.js and Tailwind.',
         site:'https://akinfemi.vercel.app/',
         code: 'https://github.com/elphie-pro/akinfemi',
         stackImages: [
             {
-                name: 'Vue Js',
                 source:  images['../assets/images/logos_vue.svg']
             },
             {
-                name: 'Nuxt JS',
                 source:  images['../assets/images/material-icon-theme_nuxt.svg']
             },
             {
-                name: 'Tailwind CSS',
                 source: images['../assets/images/devicon_tailwindcss.svg']
             },
             {
-                name: 'Framer Motion',
                 source: images['../assets/images/framer 1.svg']
             }
         ]
@@ -113,29 +108,57 @@ const projects = [
         title: 'ElphBank',
         img: images['../assets/images/Screenshot 2025-05-15 195231.png'],
         stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
-        descript: 'ElphBank is a makeshift banking site... Read more',
+        descript: 'A makeshift banking site with minimal functionalities',
         site:'',
         code: 'https://github.com/elphie-pro/ElphBank',
          stackImages: [
             {
-                name: 'yams daw rredfdwdfddfg',
-                
-            }
+                source:  images['../assets/images/nonicons_next-16 white.svg']
+            },
+            {
+                source:  images['../assets/images/Group 222.svg']
+            },
+            {
+                source: images['../assets/images/devicon_tailwindcss.svg']
+            },
+            {
+                source: images['../assets/images/devicon_firebase.svg']
+            },
+            {
+                source: images['../assets/images/framer 1.svg']
+            },
         ]
     },
     {
         title: 'ElphBlog V1',
         img: images['../assets/images/Screenshot 2025-05-15 102407.png'],
         stack: 'React.JS, JavaScript, CSS, Firebase Auth, Firestore',
-        descript: 'ElphBlog is a blogging website that allows... Read more',
+        descript: 'ElphBlog is a blogging website that allows',
         site:'https://elph-blog.vercel.app/',
-        code: 'https://github.com/elphie-pro/ElphBlog'
+        code: 'https://github.com/elphie-pro/ElphBlog',
+        stackImages: [
+            {
+                source:  images['../assets/images/nonicons_next-16 white.svg']
+            },
+            {
+                source:  images['../assets/images/Group 222.svg']
+            },
+            {
+                source: images['../assets/images/devicon_tailwindcss.svg']
+            },
+            {
+                source: images['../assets/images/devicon_firebase.svg']
+            },
+            {
+                source: images['../assets/images/framer 1.svg']
+            },
+        ]
     },
     {
         title: 'Pay4Me UI',
         img: images['../assets/images/Screenshot 2025-05-26 194523.png'],
         stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
-        descript: 'ElphBank is a makeshift banking site... Read more',
+        descript: 'ElphBank is a makeshift banking site',
         site:'',
         code: 'https://github.com/elphie-pro/Pay4Me-UI'
     },
@@ -143,7 +166,7 @@ const projects = [
         title: 'Currency Converter',
         img: images['../assets/images/Screenshot 2025-05-26 194158.png'],
         stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
-        descript: 'ElphBank is a makeshift banking site... Read more',
+        descript: 'ElphBank is a makeshift banking site',
         site:'https://currency-converter-olive-xi.vercel.app/',
         code: 'https://github.com/elphie-pro/Currency-Converter'
     },
@@ -151,7 +174,7 @@ const projects = [
         title: 'Weather App',
         img: images['../assets/images/Screenshot 2025-05-26 193925.png'],
         stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
-        descript: 'ElphBank is a makeshift banking site... Read more',
+        descript: 'ElphBank is a makeshift banking site',
         site:'https://weather-app-eight-chi-89.vercel.app/',
         code: 'https://github.com/elphie-pro/Todo-App'
     },
@@ -159,7 +182,7 @@ const projects = [
         title: 'Todo App V1',
         img: images['../assets/images/Screenshot 2025-05-26 193850.png'],
         stack: 'Next.JS, JavaScript, Tailwind CSS, Firebase Auth, Firestore, Framer Motion',
-        descript: 'ElphBank is a makeshift banking site... Read more',
+        descript: 'ElphBank is a makeshift banking site',
         site:'https://todo-app-tan-pi.vercel.app/',
         code: 'https://github.com/elphie-pro/Todo-App'
     }
